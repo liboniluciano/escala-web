103c719c6872562dc181391fdcd41b3a653b08b1
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-04 13:45:31 -0300

    Estilizacao da autenticacao com unform e validacao

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
-// import { Container } from './styles';
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
-function SignIn() {
-  return <h1>Login</h1>;
+export default function SignIn() {
+  return (
+    <>
+      <Form schema={schema}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Acessar</button>
+        <Link to="/">Esqueci minha senha</Link>
+      </Form>
+    </>
+  );
 }
-
-export default SignIn;
