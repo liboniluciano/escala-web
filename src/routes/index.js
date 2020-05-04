c9e148beabd7dfd77cca631e2f02e2de2eb23cc2
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-04 12:01:35 -0300

    Configuracao de rotas privadas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import Route from './Route';
 
 import SignIn from '../pages/SignIn';
+import Dashboard from '../pages/Dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
+      <Route path="/dashboard" exact component={Dashboard} isPrivate />
     </Switch>
   );
 }
