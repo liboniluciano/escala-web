c61cb918884764c68ed88ef811279388cd411427
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-04 21:55:05 -0300

    Mensagem de loading na aplicacao

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -3,25 +3,29 @@ import { takeLatest, call, put, all } from 'redux-saga/effects';
 import api from '../../../services/api';
 import history from '../../../services/history';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signInFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  const response = yield call(api.post, 'session', {
-    email,
-    password,
-  });
+    const response = yield call(api.post, 'session', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.admin) {
-    console.tron.log('Usuário não é administrador');
+    if (!user.admin) {
+      console.tron.log('Usuário não é administrador');
+    }
+
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    yield put(signInFailure());
   }
-
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
 export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
