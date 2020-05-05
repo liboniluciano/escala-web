e2eba69693812f69ef095d6cde4f5dabf89f44b4
Luciano Liboni <luciano_liboni@hotmail.com>
2020-05-05 10:52:10 -0300

    Exibindo toasts

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -25,7 +25,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
-    toast.error('Falha na autenticação, verifique seus dados');
+    toast.error(`Falha na autenticação, verifique seus dados`);
     yield put(signInFailure());
   }
 }
